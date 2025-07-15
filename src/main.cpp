064f8cc1e3e49a6aa50fdce5c6cbe6b70cfb5a6b
err0rgod <ndgaming458@gmail.com>
2025-07-15 19:50:09 +0530

    working on the slipper many days after

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,26 @@
 #include <Adafruit_SSD1306.h>
 #include <BLEDevice.h>
 
-
 // OLED settings
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET    -1
+#define OLED_RESET -1
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Rotary encoder pins
 #define ENC_CLK 27
-#define ENC_DT  26
-#define ENC_SW  25
+#define ENC_DT 26
+#define ENC_SW 25
 
 // Debounce and encoder timing
 #define ENC_DEBOUNCE_MS 2
-#define SW_DEBOUNCE_MS  50
+#define SW_DEBOUNCE_MS 50
 #define SW_LONGPRESS_MS 600
 
 // Menu system
-enum class ScreenState {
+enum class ScreenState
+{
   MAIN_MENU,
   BLE_MENU,
   WIFI_MENU,
@@ -34,24 +34,22 @@ enum class ScreenState {
 };
 
 #define MAX_BLE_SCAN_RESULTS 5
-BLEAdvertisedDevice* scannedDevices[MAX_BLE_SCAN_RESULTS];
+BLEAdvertisedDevice *scannedDevices[MAX_BLE_SCAN_RESULTS];
 
 int scannedDeviceCount = 0;
 
-const char* mainMenuItems[] = {
-  "BLE Attacks",
-  "WiFi Tools",
-  "NFC Tools",
-  "Settings"
-};
+const char *mainMenuItems[] = {
+    "BLE Attacks",
+    "WiFi Tools",
+    "NFC Tools",
+    "Settings"};
 const uint8_t mainMenuCount = sizeof(mainMenuItems) / sizeof(mainMenuItems[0]);
 
-const char* bleMenuItems[] = {
-  "Beacon Flood",
-  "BLE Scanner",
-  "HID Emulation",
-  "Back"
-};
+const char *bleMenuItems[] = {
+    "Beacon Flood",
+    "BLE Scanner",
+    "HID Emulation",
+    "Back"};
 const uint8_t bleMenuCount = sizeof(bleMenuItems) / sizeof(bleMenuItems[0]);
 
 // State variables
@@ -78,16 +76,19 @@ void changeScreen(ScreenState newScreen);
 void MenuNavigation(int8_t delta);
 void MenuSelect();
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   pinMode(ENC_CLK, INPUT_PULLUP);
   pinMode(ENC_DT, INPUT_PULLUP);
   pinMode(ENC_SW, INPUT_PULLUP);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
     Serial.println(F("OLED not found"));
-    for (;;);
+    for (;;)
+      ;
   }
 
   display.clearDisplay();
@@ -101,20 +102,26 @@ void setup() {
   drawMenu();
 }
 
-void loop() {
+void loop()
+{
   handleEncoder();
   handleButton();
 }
 
 // --- Rotary Encoder Handling ---
-void handleEncoder() {
+void handleEncoder()
+{
   int encA = digitalRead(ENC_CLK);
   int encB = digitalRead(ENC_DT);
 
-  if (encA != lastEncA && encA == LOW) { // Only on falling edge
-    if (encB == HIGH) {
+  if (encA != lastEncA && encA == LOW)
+  { // Only on falling edge
+    if (encB == HIGH)
+    {
       encDelta++;
-    } else {
+    }
+    else
+    {
       encDelta--;
     }
     lastEncTime = millis();
@@ -122,7 +129,8 @@ void handleEncoder() {
   lastEncA = encA;
   lastEncB = encB;
 
-  if (encDelta != 0) {
+  if (encDelta != 0)
+  {
     MenuNavigation(encDelta);
     encDelta = 0;
     drawMenu();
@@ -130,25 +138,31 @@ void handleEncoder() {
 }
 
 // --- Encoder Button Handling ---
-void handleButton() {
+void handleButton()
+{
   bool buttonState = digitalRead(ENC_SW);
   unsigned long now = millis();
 
   // Debounce
-  if (buttonState != lastButtonState) {
+  if (buttonState != lastButtonState)
+  {
     lastButtonChange = now;
     lastButtonState = buttonState;
   }
 
   // Button pressed
-  if (buttonState == LOW && (now - lastButtonChange) > SW_DEBOUNCE_MS) {
-    if (buttonPressTime == 0) {
+  if (buttonState == LOW && (now - lastButtonChange) > SW_DEBOUNCE_MS)
+  {
+    if (buttonPressTime == 0)
+    {
       buttonPressTime = now;
       buttonLongPressHandled = false;
     }
     // Long press to go back
-    if (!buttonLongPressHandled && (now - buttonPressTime) > SW_LONGPRESS_MS) {
-      if (currentScreen != ScreenState::MAIN_MENU) {
+    if (!buttonLongPressHandled && (now - buttonPressTime) > SW_LONGPRESS_MS)
+    {
+      if (currentScreen != ScreenState::MAIN_MENU)
+      {
         changeScreen(ScreenState::MAIN_MENU);
         drawMenu();
       }
@@ -156,8 +170,10 @@ void handleButton() {
     }
   }
   // Button released
-  else if (buttonState == HIGH && buttonPressTime != 0) {
-    if (!buttonLongPressHandled && (now - buttonPressTime) < SW_LONGPRESS_MS) {
+  else if (buttonState == HIGH && buttonPressTime != 0)
+  {
+    if (!buttonLongPressHandled && (now - buttonPressTime) < SW_LONGPRESS_MS)
+    {
       MenuSelect();
       drawMenu();
     }
@@ -167,31 +183,34 @@ void handleButton() {
 }
 
 // --- Free Scanned Devices --- for avoiding memory leaks
-void freeScannedDevices() {
-  for (int i = 0; i < scannedDeviceCount; i++) {
+void freeScannedDevices()
+{
+  for (int i = 0; i < scannedDeviceCount; i++)
+  {
     delete scannedDevices[i];
     scannedDevices[i] = nullptr;
   }
   scannedDeviceCount = 0;
 }
 
-
 // all functions and attacks are defined here
-void scanBLEDevices() {
+void scanBLEDevices()
+{
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println("Scanning BLE...");
   display.display();
 
   BLEDevice::init("");
-  BLEScan* pBLEScan = BLEDevice::getScan();
+  BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setActiveScan(true);
   BLEScanResults foundDevices = pBLEScan->start(3, false); // Scan for 3 seconds
 
   // Store pointers to found devices
   scannedDeviceCount = 0;
-  for (int i = 0; i < foundDevices.getCount() && scannedDeviceCount < MAX_BLE_SCAN_RESULTS; i++) {
-    BLEAdvertisedDevice* d = new BLEAdvertisedDevice(foundDevices.getDevice(i));
+  for (int i = 0; i < foundDevices.getCount() && scannedDeviceCount < MAX_BLE_SCAN_RESULTS; i++)
+  {
+    BLEAdvertisedDevice *d = new BLEAdvertisedDevice(foundDevices.getDevice(i));
     scannedDevices[scannedDeviceCount++] = d;
   }
   pBLEScan->clearResults();
@@ -233,123 +252,177 @@ void scanBLEDevices() {
   pBLEScan->clearResults();
 }*/
 
-
 // --- Menu Navigation ---
-void MenuNavigation(int8_t delta) {
+void MenuNavigation(int8_t delta)
+{
   uint8_t count = 0;
-  switch (currentScreen) {
-    case ScreenState::MAIN_MENU: count = mainMenuCount; break;
-    case ScreenState::BLE_MENU:  count = bleMenuCount;  break;
-    case ScreenState::BLE_SCAN_RESULTS: count = scannedDeviceCount; break;
-    // Add other menus here
-    default: count = mainMenuCount; break;
+  switch (currentScreen)
+  {
+  case ScreenState::MAIN_MENU:
+    count = mainMenuCount;
+    break;
+  case ScreenState::BLE_MENU:
+    count = bleMenuCount;
+    break;
+  case ScreenState::BLE_SCAN_RESULTS:
+    count = scannedDeviceCount;
+    break;
+  // Add other menus here
+  default:
+    count = mainMenuCount;
+    break;
   }
-  if (count == 0) return;
-  if (delta > 0) {
+  if (count == 0)
+    return;
+  if (delta > 0)
+  {
     selectedIndex = (selectedIndex + 1) % count;
-  } else if (delta < 0) {
+  }
+  else if (delta < 0)
+  {
     selectedIndex = (selectedIndex == 0) ? count - 1 : selectedIndex - 1;
   }
 }
 
 // --- Menu Selection ---
-void MenuSelect() {
-  switch (currentScreen) {
-    case ScreenState::MAIN_MENU:
-      switch (selectedIndex) {
-        case 0: changeScreen(ScreenState::BLE_MENU); break;
-        case 1: changeScreen(ScreenState::WIFI_MENU); break;
-        case 2: changeScreen(ScreenState::NFC_MENU); break;
-        case 3: changeScreen(ScreenState::SETTINGS_MENU); break;
-      }
+void MenuSelect()
+{
+  switch (currentScreen)
+  {
+  case ScreenState::MAIN_MENU:
+    switch (selectedIndex)
+    {
+    case 0:
+      changeScreen(ScreenState::BLE_MENU);
       break;
-    case ScreenState::BLE_MENU:
-      if (selectedIndex == 1) { // BLE Scanner
-        scanBLEDevices();
-      } else if (selectedIndex == bleMenuCount - 1) { // "Back"
-        changeScreen(ScreenState::MAIN_MENU);
-      } else {
-        // Placeholder for other BLE actions
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        display.print("Selected: ");
-        display.println(bleMenuItems[selectedIndex]);
-        display.display();
-        delay(800);
-      }
+    case 1:
+      changeScreen(ScreenState::WIFI_MENU);
       break;
-    case ScreenState::BLE_SCAN_RESULTS:
-      if (scannedDeviceCount > 0) {
-        // Device selected, do something with scannedDevices[selectedIndex]
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        display.println("Selected device:");
-        String name = scannedDevices[selectedIndex]->getName().c_str();
-        if (name.length() == 0) name = scannedDevices[selectedIndex]->getAddress().toString().c_str();
-        display.println(name);
-        display.display();
-        delay(1500);
-        // After selection, go back to BLE menu
-        changeScreen(ScreenState::BLE_MENU);
-      }
+    case 2:
+      changeScreen(ScreenState::NFC_MENU);
       break;
-    // Add other submenu logic here
-    default:
+    case 3:
+      changeScreen(ScreenState::SETTINGS_MENU);
       break;
+    }
+    break;
+  case ScreenState::BLE_MENU:
+    if (selectedIndex == 1)
+    { // BLE Scanner
+      scanBLEDevices();
+    }
+    else if (selectedIndex == bleMenuCount - 1)
+    { // "Back"
+      changeScreen(ScreenState::MAIN_MENU);
+    }
+    else
+    {
+      // Placeholder for other BLE actions
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.print("Selected: ");
+      display.println(bleMenuItems[selectedIndex]);
+      display.display();
+      delay(800);
+    }
+    break;
+  case ScreenState::BLE_SCAN_RESULTS:
+    if (scannedDeviceCount > 0)
+    {
+      // Device selected, do something with scannedDevices[selectedIndex]
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Selected device:");
+      String name = scannedDevices[selectedIndex]->getName().c_str();
+      if (name.length() == 0)
+        name = scannedDevices[selectedIndex]->getAddress().toString().c_str();
+      display.println(name);
+      display.display();
+      delay(1500);
+      // After selection, go back to BLE menu
+      changeScreen(ScreenState::BLE_MENU);
+    }
+    break;
+  // Add other submenu logic here
+  default:
+    break;
   }
 }
 
 // --- Change Screen ---
-void changeScreen(ScreenState newScreen) {
+void changeScreen(ScreenState newScreen)
+{
   currentScreen = newScreen;
   selectedIndex = 0;
 }
 
 // --- Draw Menu ---
-void drawMenu() {
+void drawMenu()
+{
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
-  const char** items = nullptr;
+  const char **items = nullptr;
   uint8_t count = 0;
-  const char* title = "";
+  const char *title = "";
 
-  switch (currentScreen) {
-    case ScreenState::MAIN_MENU:
-      items = mainMenuItems; count = mainMenuCount; title = "Main Menu"; break;
-    case ScreenState::BLE_MENU:
-      items = bleMenuItems; count = bleMenuCount; title = "BLE Attacks"; break;
-    case ScreenState::BLE_SCAN_RESULTS:
-      title = "Select Device";
-      count = scannedDeviceCount;
-      break;
-    // Add other menus here
-    default:
-      items = mainMenuItems; count = mainMenuCount; title = "Menu"; break;
+  switch (currentScreen)
+  {
+  case ScreenState::MAIN_MENU:
+    items = mainMenuItems;
+    count = mainMenuCount;
+    title = "Main Menu";
+    break;
+  case ScreenState::BLE_MENU:
+    items = bleMenuItems;
+    count = bleMenuCount;
+    title = "BLE Attacks";
+    break;
+  case ScreenState::BLE_SCAN_RESULTS:
+    title = "Select Device";
+    count = scannedDeviceCount;
+    break;
+  // Add other menus here
+  default:
+    items = mainMenuItems;
+    count = mainMenuCount;
+    title = "Menu";
+    break;
   }
 
   display.setCursor(0, 0);
   display.println(title);
 
-  if (currentScreen == ScreenState::BLE_SCAN_RESULTS) {
-    for (uint8_t i = 0; i < count; i++) {
+  if (currentScreen == ScreenState::BLE_SCAN_RESULTS)
+  {
+    for (uint8_t i = 0; i < count; i++)
+    {
       display.setCursor(0, 14 + i * 11);
-      if (i == selectedIndex) display.print("> ");
-      else display.print("  ");
+      if (i == selectedIndex)
+        display.print("> ");
+      else
+        display.print("  ");
       String name = scannedDevices[i]->getName().c_str();
-      if (name.length() == 0) name = scannedDevices[i]->getAddress().toString().c_str();
+      if (name.length() == 0)
+        name = scannedDevices[i]->getAddress().toString().c_str();
       display.println(name);
     }
-    if (count == 0) {
+    if (count == 0)
+    {
       display.setCursor(0, 14);
       display.println("None found.");
     }
-  } else {
-    for (uint8_t i = 0; i < count; i++) {
+  }
+  else
+  {
+    for (uint8_t i = 0; i < count; i++)
+    {
       display.setCursor(0, 14 + i * 11);
-      if (i == selectedIndex) display.print("> ");
-      else display.print("  ");
+      if (i == selectedIndex)
+        display.print("> ");
+      else
+        display.print("  ");
       display.println(items[i]);
     }
   }
